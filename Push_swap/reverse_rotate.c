0675e39932ae42f14af2ca19a027783b18458a81
Mohamed El adnani <mohamedeladnani0@gmail.com>
2025-01-20 06:08:59 -0800

    Add files via upload

---
+++ Push_swap/reverse_rotate.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-adna <mel-adna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 15:01:47 by mel-adna          #+#    #+#             */
+/*   Updated: 2025/01/20 11:48:18 by mel-adna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	reverse_rotate(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	last = *stack;
+	while (last->next)
+		last = last->next;
+	*stack = last;
+	last->next = first;
+	first->prev = last;
+	last->prev->next = NULL;
+	last->prev = NULL;
+}
+
+void	rra(t_stack **a)
+{
+	reverse_rotate(a);
+	ft_putstr_fd("rra\n", 1);
+}
+
+void	rrb(t_stack **b)
+{
+	reverse_rotate(b);
+	ft_putstr_fd("rrb\n", 1);
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	reverse_rotate(a);
+	reverse_rotate(b);
+	ft_putstr_fd("rrr\n", 1);
+}
